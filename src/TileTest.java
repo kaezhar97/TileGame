a9cb5f18552663d07a394a09e3a43bd011699d51
Octavio Avila-Cardet <oavil004@fiu.edu>
2017-09-07 13:10:40 -0400

    Added user prompt to the main class. The user can now choose to see multiple games

--- src/TileTest.java
+++ src/TileTest.java
@@ -2,6 +2,8 @@
  * Test class for tile game
  */
 
+import javax.swing.JOptionPane ;
+
 /**
  *
  * @author Octavio Avila-Cardet
@@ -9,11 +11,19 @@
 public class TileTest {
     public static void main (String args [])
     {
-       
+       String input;
+       do{
         TileGame startGame = new TileGame ();
         startGame.play();
         System.out.println(startGame);
        
-      
+        input = JOptionPane.showInputDialog("Would you like to see another game?\nType \"y\" or \"n\"") ;
+        
+        while (!(input.toLowerCase().equals("y")||input.toLowerCase().equals("n")))
+        {
+            input = JOptionPane.showInputDialog("Please enter a valid input\n\nWould you like to see another game?\nType \"y\" or \"n\"") ;
+        }
+        
+       }while(input.toLowerCase().equals("y"));
     }
 }
