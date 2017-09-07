3582bb4b4f880cc436e9f939e46dccab53c167f9
Octavio Avila-Cardet <oavil004@fiu.edu>
2017-09-07 13:39:06 -0400

    Added a lot of documentation to the Hand.java file

--- src/Hand.java
+++ src/Hand.java
@@ -1,40 +1,54 @@
 import java.util.ArrayList;
 
+/*
+File: Hand.java
+Purpose: 
+   This class simulates a real world hand full of tiles. The starting hand consists of 5 tiles
+*/
+
+
 /**
  * A Hand is a collection of NumberTiles.  Tiles may be removed from 
  * the Hand and new tiles added to it
+ * 
+ * @author Octavio Avila-Cardet
  */
 public class Hand
 {
-    private ArrayList<NumberTile> hand ;
-    private static int HAND_SIZE = 5 ;          // starting hand size
+    private ArrayList<NumberTile> hand ;  // Creates an ArrayList consisting of tiles. This is the main variable used to represent the hand
+    private static int HAND_SIZE = 5 ;   // starting hand size
     
     /**
      * Create a new Hand of HAND_SIZE tiles
      */
     public Hand()
     {
-       hand=new ArrayList<>();
-       NumberTile newTile;
+       hand=new ArrayList<>(); //An empty list of tiles is added to the hand
+       NumberTile newTile; //A new tile pointer is created
+       
+       //This for loop is used to populate the hand with tiles
        for (int i=0;i<HAND_SIZE;i++)
        {
-         newTile=new NumberTile();
-         hand.add(newTile);
+         newTile=new NumberTile();// During every iteration of the loop, a new tile is created
+         hand.add(newTile);//Then, the tile is added to the hand. This process is repeated HAND_SIZE times. In this way, 5 tiles are added to the hand
        }
   
     }
     
     /**
-     * Create a new Hand as an exact duplicate (i.e. a "deep copy") of another
+     * Create a new Hand as an exact duplicate (i.e. a "deep copy") of another.
+     * The purpose of this is to save the starting hand to be displayed to the user after the game is over.
+     * We need to save a copy of the starting hand because it will be affected throughout the game.
      * @param toBeCopied the Hand to be copied
      */
     public Hand(Hand toBeCopied)
     {
-        hand = new ArrayList<>();
+        hand = new ArrayList<>(); //An empty list is added to the hand that will be populated later with values of the original hand
         
+        //This for loop is used to populate the hand with all the tiles from the original hand
         for (int i=0;i<toBeCopied.getSize();i++)
         {
-            hand.add(toBeCopied.get(i));
+            hand.add(toBeCopied.get(i)); //Grab each tile from the original hand and put it in the copy. In this way, a deep copy is achieved.
         }
  
     }
@@ -46,9 +60,9 @@ public class Hand
      */
     public NumberTile get(int index)
     {
-       // temporary return statement so skeleton will compile and run
-       NumberTile tileToShow;
-       tileToShow=hand.get(index);
+       
+       NumberTile tileToShow; //Creates a tile that will be returned by the method later
+       tileToShow=hand.get(index); //The created tile is given the value of the tile at the specified index
         
        return tileToShow;
     }
@@ -59,8 +73,6 @@ public class Hand
      */
     public int getSize()
     {
-       
-       // temporary return statement so skeleton will compile and run
        return hand.size() ;
     }
     
@@ -69,15 +81,14 @@ public class Hand
      */
     public void addTile()
     {
-        NumberTile newTile;
-        newTile=new NumberTile();
-        hand.add(newTile);
+        NumberTile newTile; //A new tile is created
+        newTile=new NumberTile(); //The tile is given a random tile
+        hand.add(newTile); //The random tile is added to the hand
     }
     
     /**
      * Remove the tile at a specified index (position) from this Hand 
      * @param index the index (position) of the tile to be removed
-     *
      */
     public void removeTile(int index)
     {
@@ -87,14 +98,13 @@ public class Hand
     /**
      * Is this Hand empty? 
      * @return true if this Hand is empty (contains no tiles); otherwise false
-     *
      */
     public boolean isEmpty()
     {
        // temporary return statement so skeleton will compile and run
         
-       boolean isEmpty;
-       isEmpty=hand.size()==0;
+       boolean isEmpty; // Variable to check if the hand is empty
+       isEmpty=hand.size()==0; //Checks if the hand is empty and stores the result in the boolean variable
 
        return isEmpty ;
     }
