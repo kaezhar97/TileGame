ef25a75fd88b4c569741f66a6117ef9afad18843
Octavio Avila-Cardet <oavil004@fiu.edu>
2017-09-07 14:49:49 -0400

    Added javadoc and a sample output file

--- src/TileTest.java
+++ src/TileTest.java
@@ -6,9 +6,13 @@ import TileGameClasses.TileGame;
 import javax.swing.JOptionPane ;
 
 /**
+ * The main class for the tile game.
+ * It plays a tile game. Then, asks the user if he or she wants to see another one.
+ * 
  * @author Octavio Avila-Cardet
  */
 public class TileTest {
+    
     public static void main (String args [])
     {
        System.out.println("**************************");
@@ -21,13 +25,14 @@ public class TileTest {
         System.out.println("STARTING NEW GAME");
         System.out.println("-----------------");
         
-        TileGame startGame = new TileGame ();
-        startGame.play();
-        System.out.println(startGame);
+        TileGame startGame = new TileGame (); //Starts a new tile game
+        startGame.play(); // Plays the game
+        System.out.println(startGame); //Prints out the result of the game
        
-        input = JOptionPane.showInputDialog("Would you like to see another game?"
+        input = JOptionPane.showInputDialog("Would you like to see another game?" //Ask the user if they want to see another game
                 + "\nType \"y\" or \"n\"") ;
         
+        //Ensures that the user is inputting valid answers
         while (!(input.toLowerCase().equals("y")||input.toLowerCase().equals("n")))
         {
             input = JOptionPane.showInputDialog("Please enter a valid input\n"
@@ -35,7 +40,7 @@ public class TileTest {
                     + "\nType \"y\" or \"n\"") ;
         }
         
-       }while(input.toLowerCase().equals("y"));
+       }while(input.toLowerCase().equals("y")); //Keeps playing new games for as long as the user keeps answering "y" to the prompt
        
         System.out.println("\n**********************");
         System.out.println("*THE GAMES HAVE ENDED*");
